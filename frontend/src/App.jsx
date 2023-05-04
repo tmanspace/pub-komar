1dd799ee156e9f5cb73c8489b9646a50df71e230
Tim <serazutdinov.tim@gmail.com>
2023-05-04 03:35:58 +0300

    Initial layout

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+
+import { Navbar, Header } from './components'
+
+const App = () => {
+  return (
+    <div className='bg-primary w-full overflow-hidden'>
+      <Navbar />
+      <Header />
+      {/* <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer /> */}
+    </div>
+  )
+}
+
+export default App;
---
+++ frontend/src/components/index.js
@@ -0,0 +1,8 @@
+import Navbar from "./Navbar";
+import Header from "./Header";
+
+
+export {
+    Navbar,
+    Header
+}
